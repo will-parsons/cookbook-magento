5469088b97685ad70ad5b8ac169081334a7e0353
Brint O'Hearn <brint.ohearn@rackspace.com>
2013-07-24 14:19:16 -0500

    Updated comments

--- recipes/firewall-memcached.rb
+++ recipes/firewall-memcached.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: magento
-# Recipe:: firewall
+# Recipe:: firewall-memcached
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
