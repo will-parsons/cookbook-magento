aa0081f9153c639adff330a166c85c9dbfc80bf9
Brint O'Hearn <brint.ohearn@rackspace.com>
2013-07-26 16:12:29 -0500

    re-ordering operations to fix permissions issue

--- recipes/default.rb
+++ recipes/default.rb
@@ -148,10 +148,6 @@ unless File.exist?("#{node[:magento][:dir]}/.installed")
   db = node[:magento][:db]
   mysql = node[:mysql]
 
-  if !File.exist?("#{node[:magento][:dir]}/app/etc/local.xml") && !Magento.tables_exist?(mysql[:bind_address], db[:username], db[:password], mysql[:port]) 
-    magento_initial_configuration
-  end
-
   bash "Ensure correct permissions & ownership" do
     cwd node[:magento][:dir]
     code <<-EOH
@@ -159,6 +155,10 @@ unless File.exist?("#{node[:magento][:dir]}/.installed")
     chmod -R o+w media
     chmod -R o+w var
     EOH
+  end
+
+  if !File.exist?("#{node[:magento][:dir]}/app/etc/local.xml") && !Magento.tables_exist?(mysql[:bind_address], db[:username], db[:password], mysql[:port])
+    magento_initial_configuration
   end
 
   # Install and configure varnish
